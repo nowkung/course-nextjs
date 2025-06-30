c6639671487b40eae681f3fda198648228d35ab1
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-06-30 16:24:16 +0700

    Hero function component

--- 01-hello-react/src/Heroes.tsx
+++ 01-hello-react/src/Heroes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, use } from 'react';
 
 
 type HeroProps = {
@@ -17,52 +17,105 @@ const Hero = ({ name, power, powerName }: HeroProps) => {
   )
 }
 
-const heroes = [
-  {
-    "name": "Thor",
-    "power": 5000,
-    "powerName": "Lightning"
-  },
-  {
-    "name": "IronMan",
-    "power": 300,
-    "powerName": "Arc reactor"
-  },
-  {
-    "name": "DoctorStrange",
-    "power": 4500,
-    "powerName": "Magic"
-  }
-]
-
-type HeroesProps = {
-}
-
 type HeroState = {
     heroes: HeroProps[];
+    isLoading: boolean;
 }
 
-export class Heroes extends React.Component<HeroesProps, HeroState> {
+export const Heroes = () => {
+  const [heroes, setHeroes] = useState<HeroProps[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    constructor(props: HeroProps) {
+  useEffect(() => {
+    fetch('http://localhost:2727/heroes')
+        .then(response => response.json())
+        .then(data => {
+          setHeroes(data);
+        })
+        .catch(error => {
+          console.error('Error fetching heroes:', error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+
+        console.log('Heroes Once: ');
+  }, []);
+
+  useEffect(() => {
+    console.log('Heroes updated:xxxx' );
+  })
+
+  return (
+    <>
+      {
+        isLoading ? <p>Loading heroes...</p> :
+        <div className="heroes-list">
+          <h1>Heroes List</h1>
+          {
+            heroes.map((hero, index) => (
+              <Hero key={index} name={hero.name} power={hero.power} powerName={hero.powerName} />
+            ))
+          }
+        </div>
+
+
+      }
+
+       <button onClick={() => {
+          const newHero: HeroProps = {
+            name: 'New Hero',
+            power: 100,
+            powerName: 'Super Strength'
+          };
+          setHeroes([...heroes, newHero]);
+        }}>
+          Add Hero
+        </button>
+    </>
+  );
+}
+
+export class Heroesx extends React.Component<{}, HeroState> {
+
+    constructor(props: {}) {
         super(props);
         // Initialize state if needed
         this.state = {
-            heroes: heroes
+            heroes: [],
+            isLoading: true
         };
     }
 
+    componentDidMount(): void {
+      fetch('http://localhost:2727/heroes')
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ heroes: data });
+        })
+        .catch(error => {
+          console.error('Error fetching heroes:', error);
+        })
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
+    }
 
   render() {
     return (
         <>
+          {
+          this.state.isLoading ? <p>Loading heroes...</p> :
+
+          <div className="heroes-list">
             <h1>Heroes List</h1>
             {
                 this.state.heroes.map((hero, index) =>(
                     <Hero key={index} name={hero.name} power={hero.power} powerName={hero.powerName} />
                   ))
             }
-
+          </div>
+          }
         </>
         );
   }
